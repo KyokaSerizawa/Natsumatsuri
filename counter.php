8f984d0cb1c03b36e24111b282bc4c78ba9154b9
Tyler Tirapelle <152163132+Shogunoftheitsoffice@users.noreply.github.com>
2025-07-16 00:47:31 +0900

    Create counter.php

---
+++ counter.php
@@ -0,0 +1,26 @@
+<?php
+
+function update_and_get_visitor_count() {
+    $counter_file = 'counter.txt';
+    $count = 0;
+
+    // Open the counter file for reading and writing, and create it if it doesn't exist.
+    // The file lock (flock) prevents multiple users from accessing the file at the
+    // exact same time, which could corrupt the data.
+    $fp = fopen($counter_file, 'c+');
+    if (flock($fp, LOCK_EX)) { // Get an exclusive lock
+        $current_count_str = fread($fp, filesize($counter_file) ?: 1);
+        $count = intval($current_count_str) + 1;
+
+        ftruncate($fp, 0); // Erase the file content
+        rewind($fp);      // Move the file pointer to the beginning
+        fwrite($fp, $count); // Write the new count
+
+        flock($fp, LOCK_UN); // Release the lock
+    }
+    fclose($fp);
+
+    return $count;
+}
+
+?>
